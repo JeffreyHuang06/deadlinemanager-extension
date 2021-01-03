e8f5980f6600382f2ee31d62b418abc5ce72f285
FeverDreme <jeffreyhuang08@gmail.com>
2021-01-03 04:27:52 -0500

    added individual state for both
     added react fragment so setinterval func wouldnt clutter

--- src/MainDeadline.jsx
+++ src/MainDeadline.jsx
@@ -4,27 +4,33 @@ import {retrieveSortedJSONList, retrieveNearestDate} from './chromeAPI/retrieveD
 
 export default function MainDeadline() {
     // get the date and the stuff from chrome storage
-    let [date, setDate] = useState("Hello");
+    let [school, setSchool] = useState("");
+    let [date, setDate] = useState("");
 
     async function fetchData() {
         let res = await retrieveSortedJSONList();
         let nearest = await retrieveNearestDate();
-        // console.log(res);
 
         // parse the nearest date
         let len = nearest.length;
         if (len === 1){
-            // update the state
+            setSchool(nearest[0].school);
+            setDate(nearest[0].date);
         }
+
     }
     function keepfetching() {
         setInterval(fetchData, 1000);
     }
 
     return (
-        <div className="MainDeadline">
-            {date}
+        <>
+            <div className="MainDeadline">
+                <div className='school'>{school}</div>
+                <div classNmae='date'>{date}</div>
+            </div>
+
             {keepfetching()}
-        </div>
+        </>
     )
 }
