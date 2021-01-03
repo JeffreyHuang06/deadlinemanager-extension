03d4b34222b820fdf1a1a9f5d6ed51f266f304c0
FeverDreme <jeffreyhuang08@gmail.com>
2021-01-02 23:34:48 -0500

    created testing and new data struct

--- public/background.js
+++ public/background.js
@@ -1,14 +1,27 @@
-// let chrome = require("chrome"); // just so the linter doesn't freak out on me
-
+/*global chrome*/
 // key : deadlinelist
 // value: another json with key:value => place:deadline
 
 // there will only be one chrome storage
 
 chrome.runtime.onInstalled.addListener(function() {
+    let ddlist_temp_obj = [
+        {
+            "school": "Exeter",
+            "date": (new Date(2021,0,15)).toString()
+        },
+        {
+            "school": "Andover",
+            "date": (new Date(2021,1,1)).toString()
+        }, {
+            "school": "dummy school",
+            "date": (new Date(2021,0,15)).toString()
+        }
+    ];
 
-    chrome.storage.sync.set({deadlinelist: {}}, () => {
+    chrome.storage.sync.set({deadlinelist: ddlist_temp_obj}, () => {
         console.log("Successfully instantiated deadlinelist");
+        console.log(ddlist_temp_obj);
     });
 });
 
