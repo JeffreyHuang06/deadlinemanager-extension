06cb265914c99c1dbd697a91a86f4c7d2d1696b6
FeverDreme <jeffreyhuang08@gmail.com>
2021-01-03 20:22:24 -0500

    added form validation but no actual submission

--- src/InputForm.jsx
+++ src/InputForm.jsx
@@ -1,15 +1,56 @@
 import React from 'react'
 import './css/InputForm.css'
+import {checkList} from './chromeAPI/retrieveSchoolList'
 
 export default class InputForm extends React.Component {
     state = {
         inputschool: "",
-        inputdate: ""
+        inputdate: "",
+        invalidschool: false,
+        invaliddate: false,
+        badschool: "",
+        baddate: ""
     }
 
-    onSubmit = event => {
+    validateForm = async () => {
+        // check to make sure its not in the list
+        let valschool = await checkList(this.state.inputschool);
+        if (!valschool)
+            this.setState({
+                invalidschool: true,
+                badschool: this.state.inputschool
+            });
+        else 
+            this.setState({
+                invalidschool: false,
+                badschool: ""
+            });
+
+        // check to make sure the date hasnt been reached yet
+        let now = Date.now();
+        let checkDate = new Date(this.state.inputdate);
+
+        if (checkDate.getTime() < now)
+            this.setState({
+                invaliddate: true,
+                baddate: this.state.inputdate
+            });
+        else
+            this.setState({
+                invaliddate: false,
+                baddate: ""
+            });
+
+        return !this.state.invalidschool && !this.state.invaliddate; // want both to be valid
+    }
+
+    handleSubmit = async(event) => {
         event.preventDefault();
-        console.log(this.state);
+        
+        // validate the form
+        if (await this.validateForm()){
+            console.log('this is good');
+        } else console.log('this is bad');
     }
 
     handleChange = (event, fieldname) => {
@@ -18,7 +59,7 @@ export default class InputForm extends React.Component {
 
     render() {
         return (
-            <form className='InputForm' onSubmit={this.onSubmit}>
+            <form className='InputForm' onSubmit={this.handleSubmit}>
                 <h3>Enter New School</h3>
 
                 <div id='inputname' className='Inputer'>
@@ -29,6 +70,7 @@ export default class InputForm extends React.Component {
                         value={this.state.inputschool}
                         required 
                     />
+                    {this.state.invalidschool ? <p>{this.state.badschool} is already registered</p> : null}
                 </div>
 
                 <div id='inputdate' className='Inputer'>
@@ -39,6 +81,7 @@ export default class InputForm extends React.Component {
                         value={this.state.inputdate}
                         required
                     />
+                    {this.state.invaliddate ? <p>{this.state.baddate} is invalid</p> : null}
                 </div>
 
                 <div id='inputsubmit' className='Inputer'>
