896c07ce9b17b2eb1df284cf251a56805ee4615b
FeverDreme <jeffreyhuang08@gmail.com>
2021-01-05 03:03:47 -0500

    UPDATE: added in schoollist handler

--- src/chromeAPI/storeNewDeadline.ts
+++ src/chromeAPI/storeNewDeadline.ts
@@ -1,18 +1,23 @@
 /*global chrome*/
 import {retrieveJSONList} from './retrieveDeadlineJSON'
+import {retrieveSchoolList} from './retrieveSchoolList'
 
 export async function storeNewDeadline(school: string, date: string){
     // first we need to get it
     // then we push back
     // then we do the thing
-    let res = await retrieveJSONList();
-    res.push({
+    let retrievedDeadlineList = await retrieveJSONList();
+    retrievedDeadlineList.push({
         "school": school,
         "date": date
     });
+
+    let retrievedSchoolList = await retrieveSchoolList();
+    retrievedSchoolList.push(school);
     
     //@ts-ignore
     chrome.storage.sync.set({
-        "deadlinelist": res
+        "deadlinelist": retrievedDeadlineList,
+        "schoollist": retrievedSchoolList
     });
 }
