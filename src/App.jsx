8a24c83608574db860229949d62d7fd72e9a7ccd
FeverDreme <jeffreyhuang08@gmail.com>
2021-01-05 03:02:59 -0500

    CHANGE: refactored for components

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
-import MainDeadline from './MainDeadline'
-import InputForm from './InputForm'
+import MainDeadline from './components/MainDeadline'
+import InputForm from './components/InputForm'
+
+// make all componets classes expect for those that useState
+// then use the <App /> to handle ALL STATE because it's all intertwined. I'm probably gonna put this on my big monitor
 
 export default function App() {
   return (
