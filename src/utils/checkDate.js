098d4d5bb1914097411cf5fd489cccfbcd4210fb
FeverDreme <jeffreyhuang08@gmail.com>
2021-01-05 03:04:37 -0500

    NEW: date validation offloaded to separate file

---
+++ src/utils/checkDate.js
@@ -0,0 +1,13 @@
+export function checkDate(date){
+    let d = new Date(date);
+    let fixeddate = new Date(
+        d.getFullYear(),
+        d.getMonth(),
+        d.getDate() + 1, // because its 0 indexed
+        23,
+        59,
+        59
+    );
+
+    return fixeddate.getTime() >= Date.now();
+}
