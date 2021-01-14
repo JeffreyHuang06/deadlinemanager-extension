cd4f2ce791ba5aa1def319b382be55fccfcd6527
FeverDreme <jeffreyhuang08@gmail.com>
2021-01-14 04:29:39 -0500

    MISC: added notes about heap implemetation

--- src/states/sorteddeadlinelist.js
+++ src/states/sorteddeadlinelist.js
@@ -3,6 +3,9 @@ import {sortJSONList} from '../chromeAPI/retrieveDeadlineJSON'
 import {selector} from 'recoil'
 import DeadlineList from './deadlinelist'
 
+// give this a set command in order to add values
+// make this keep min heap with an atom so it cna do it in O(logn) rather than O(nlogn)
+
 const SortedDeadlineList = selector({
     key: "sorteddeadlinelist",
     get: ({get}) => {
