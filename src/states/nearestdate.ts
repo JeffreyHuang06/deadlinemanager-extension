39b1c9883e1a27521adefc45f1983977f40c6ab0
FeverDreme <jeffreyhuang08@gmail.com>
2021-01-15 01:38:37 -0500

    REFACTOR: added types

---
+++ src/states/nearestdate.ts
@@ -0,0 +1,53 @@
+import {selector} from 'recoil'
+import DeadlineList from './deadlinelist'
+import {getNearestDate} from '../chromeAPI/retrieveDeadlineJSON'
+import { RecoilValueReadOnly } from 'recoil';
+
+interface NearestDateSelector {
+    schools: string[],
+    dates: string[]
+}
+
+const NearestDate: RecoilValueReadOnly<NearestDateSelector> = selector({
+    key: "nearestdate",
+    get: ({get}) => {
+
+        // first retrieve the atom state
+        const reslist = get(DeadlineList);
+        const nearest = getNearestDate(reslist);
+        const len: number = nearest.length;
+
+        let schools: string[];
+        let dates: string[];
+
+        if (len === 0){
+            schools = ["No schools registered"];
+            dates = [""];
+
+        } else if (len === 1){
+            schools = [nearest[0].school];
+            dates = [nearest[0].date];
+            
+        } else if (len <= 16) {
+            // we need to render a lot of stuff
+            schools = [];
+            dates = [];
+
+            for (const elem of nearest) {
+                schools.push(elem.school);
+                dates.push(elem.date);
+            }
+
+        } else {
+            schools = ["error"];
+            dates = ["error"];
+        }
+
+        return {
+            schools,
+            dates
+        };
+    }
+});
+
+export default NearestDate;
