ad68dcc609e43aeb80d4e6cf2f0925018b8ef4e4
FeverDreme <jeffreyhuang08@gmail.com>
2021-01-20 14:28:11 -0500

    REMOVED DEFAULTS

--- public/background.ts
+++ public/background.ts
@@ -5,31 +5,11 @@
 // there will only be one chrome storage
 
 chrome.runtime.onInstalled.addListener(function() {
-    let ddlist_temp_obj = [
-        {
-            "school": "Exeter",
-            "date": "2021-01-15"
-        },
-        {
-            "school": "Andover",
-            "date": "2021-02-01"
-        }
-    ];
-
-    let ddlistnames_temp_obj = [
-        "Exeter",
-        "Andover"
-    ];
-
-    let ddlistbools_temp_obj = {
-        "Exeter": true,
-        "Andover": false
-    };
 
     chrome.storage.sync.set({
-        "deadlinelist": ddlist_temp_obj,
-        "schoolist": ddlistnames_temp_obj,
-        "schoolstatelist": ddlistbools_temp_obj,
+        "deadlinelist": [],
+        "schoolist": [],
+        "schoolstatelist": {},
         "showdates": true,
         "showcomplete": true
     });
